a0e9b0692a5593af2d946226350e9a6f48a3ed76
Klassen.Olga <lefelog@mail.ru>
2019-11-03 18:37:02 +0100

    bla

--- src/main/java/de/bord/festival/Main.java
+++ src/main/java/de/bord/festival/Main.java
@@ -4,7 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main classblabla");
+        System.out.println("i am on dev branch");
     }
 
 }
