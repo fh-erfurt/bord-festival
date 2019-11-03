e9da676ee8d68cb04206eb34e1c9b52f999b955c
Klassen.Olga <lefelog@mail.ru>
2019-11-03 18:23:06 +0100

    main class renamed

--- src/main/java/de/bord/festival/Main.java
+++ src/main/java/de/bord/festival/Main.java
@@ -2,9 +2,9 @@ package de.bord.festival;
 
 import org.w3c.dom.ls.LSOutput;
 
-public class Domain {
+public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Main class");
     }
 
 }
