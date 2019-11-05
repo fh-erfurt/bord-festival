380667628be6324120828c2ed52ed33451de279f
Klassen.Olga <lefelog@mail.ru>
2019-11-05 17:07:35 +0100

    test example

--- src/main/java/de/bord/festival/Main.java
+++ src/main/java/de/bord/festival/Main.java
@@ -3,9 +3,10 @@ package de.bord.festival;
 import org.w3c.dom.ls.LSOutput;
 
 public class Main {
-    public String giveHello() {
-       return "Junit Hello WOrld dependency framework-- maven";
+    public static void main(String[] args) {
+
     }
+
 
 }
 
--- src/test/java/de/bord/festival/MainTest.java
+++ src/test/java/de/bord/festival/MainTest.java
@@ -5,15 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
-    @Test
-    public void should_give_hello_world(){
-        //Given
-        Main main1=new Main();
-        //when
-        String hello=main1.giveHello();
-        //Then
-        assertEquals("Junit Hello WOrld dependency framework-- maven", hello);
-    }
+
 
 
 
