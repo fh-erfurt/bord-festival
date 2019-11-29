85430f9fc07a707ce6ddc4eb5c8bdb0e3fd36fd0
Daniel <Daniel@DESKTOP-KJBOI1M>
2019-11-29 01:23:20 +0100

    added Tests for ClientNameExceptions

--- src/test/java/de/bord/festival/client/ClientTest.java
+++ src/test/java/de/bord/festival/client/ClientTest.java
@@ -1,7 +1,7 @@
 package de.bord.festival.client;
 
 import de.bord.festival.address.Address;
-import de.bord.festival.exception.BudgetException;
+import de.bord.festival.exception.ClientNameException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,16 +12,26 @@ public class ClientTest {
 
     String phoneNumber = "123";
     int id = 999;
+    String firstname = "a";
+    String lastname = "b";
 
     Address address = new Address("Deutschland", "Jena", "Anger 1", "07745");
+    Client client = new Client(firstname, lastname, phoneNumber, id, address);
+
+    public ClientTest() throws ClientNameException {
+    }
 
     @Test
-    void should_throw_exception_for_name_with_specialchars() {
-        /*
+    void should_throw_exception_for_name_with_specialchars() throws ClientNameException {
         String testname = "!";
-        Client client = new Client(testname, phoneNumber, id, address);
-        client.validateName();
-        assertThrows(BudgetException, client.validateName());
-         */
+
+        //assertThrows(ClientNameException.class, client.nameCheck(testname));
+    }
+
+    @Test
+    void should_return_true_for_valid_name() {
+        String testname = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZäöüÄÖÜß";
+
+        //assertEquals(ClientNameException.class, client.nameCheck(testname));
     }
 }
