4b2bd89d6872feefcdb82ce1862e1dc088d62379
Daniel Depta <56829289+ddepta@users.noreply.github.com>
2020-07-16 00:25:25 +0200

    added fragments for head, header and footer. implemented fragments into events

--- src/main/java/de/bord/festival/WebSecurityConfig.java
+++ src/main/java/de/bord/festival/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class    WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/*", "/js/**", "/css/**", "/images/**").permitAll()
+                .antMatchers("/*", "fragments/**", "/js/**", "/css/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
